539a001637fa0defc49e1e341c7d62bf01888e21
Abdelrahman Shahin <abdelrahman.shahin@stud.hs-bochum.de>
2022-04-09 14:59:12 +0200

    verbindung frondend backend

--- koelender/koelender/settings.py
+++ koelender/koelender/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,7 +58,9 @@ ROOT_URLCONF = 'koelender.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'frontend/build'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,3 +136,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'frontend/build/static'),
+]
--- koelender/koelender/urls.py
+++ koelender/koelender/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
+from . import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('api.urls')),
+    path('api/',include('api.urls')),
+    #path('', index.webseite),
+    path('',TemplateView.as_view(template_name='index.html')),
+
 ]
