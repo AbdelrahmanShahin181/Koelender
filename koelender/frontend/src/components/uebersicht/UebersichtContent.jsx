39c8c24ed175892c42a1695b5d88f639aa2b77f2
AbdelrahmanShahin <abdelrahman.shahin@stud.hs-bochum.de>
2022-04-20 12:27:07 +0200

    commit bla bla bla

--- koelender/frontend/src/components/uebersicht/UebersichtContent.jsx
+++ koelender/frontend/src/components/uebersicht/UebersichtContent.jsx
@@ -139,14 +139,10 @@ export default class KalenderContent extends React.Component{
                             //console.log(this.state.aktiveFilter[j]);
                             //console.log(pruefungenValues[j]);
                         }
-<<<<<<< HEAD
                         if(pruefungenValues[j]==null){
                             pruefungenValues[j]="Abdo"
                         }
                         if(pruefungenValues[j].toString().match(this.state.searchItem)){
-=======
-                        if(pruefungenValues[j].toString().toLowerCase().match(this.state.searchItem.toLowerCase())){
->>>>>>> 5332f13672546741a7aff44fd34c81a6c1f16771
                             containsSearch = true;
                             /*console.log(containsSearch);
                             console.log(this.state.searchItem)
