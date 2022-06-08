0cdb13fa1ca8f0dd0baf5e5d94deffa76e7186c5
AbdelrahmanShahin <abdelrahman.shahin@stud.hs-bochum.de>
2022-06-08 11:30:35 +0200

    Datenbank auf SQLite3

--- koelender/koelender/settings.py
+++ koelender/koelender/settings.py
@@ -100,19 +100,19 @@ WSGI_APPLICATION = 'koelender.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-   # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+   }
 
-  'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'koelender',
-         'USER': 'root',
-         'PASSWORD':'root',
-         'HOST': 'localhost',
-         'PORT': '3306',
-     }
+  #'default': {
+   #      'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'koelender',
+     #    'USER': 'root',
+      #   'PASSWORD':'root',
+       #  'HOST': 'localhost',
+        # 'PORT': '3306',
+     #}
 }
 
 
